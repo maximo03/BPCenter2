99fc4700212db2b89eceb329f4f1a16153c3a578
maximo03 <nestorrzv@gmail.com>
2026-05-16 12:56:07 -0600

    se agregan imagenes y stilo al menu lateral

--- BPCenter2/Models/DTOs/LoginDTO.cs
+++ BPCenter2/Models/DTOs/LoginDTO.cs
@@ -5,7 +5,11 @@ namespace BPCenter2.Models.DTOs;
 public class LoginDTO
 {
     public string? userFullName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Por favor, debe escribir un correo!..")]
     public string? userEmail { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Por favor, debe escribir una contraseña!..")]
     public string? userPassword { get; set; } = string.Empty;
     public string? userRole { get; set; } = string.Empty;
     public bool IsAuthenticated { get; set; }
